242185c16e7a199206260455f8adc2c34c6492f1
Ramon Tayag <ramon.tayag@gmail.com>
2010-05-31 21:04:36 +0800

    added more instructions, updated container.rb

--- lib/container.rb
+++ lib/container.rb
@@ -1,5 +1,14 @@
 class Container < ActiveRecord::Base
-  acts_as_ordered_tree
-  belongs_to :containable, :polymorphic => true
-  default_scope :order => "position"
+	acts_as_ordered_tree
+	belongs_to :containable, :polymorphic => true
+	default_scope :order => "position"
+	before_destroy :destroy_containable
+
+	private
+
+	def destroy_containable
+		if containable_type.constantize.exists?(containable_id)
+			containable_type.constantize.destroy(containable_id) 
+		end
+	end
 end
