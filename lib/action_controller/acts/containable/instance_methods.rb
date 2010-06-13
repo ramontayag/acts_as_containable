6153c84363e2f35979a1e919db53cfb59f9562a2
Ramon Tayag <ramon.tayag@gmail.com>
2010-06-13 18:07:23 +0800

    allowed ajax response for creation of container and containable

--- lib/action_controller/acts/containable/instance_methods.rb
+++ lib/action_controller/acts/containable/instance_methods.rb
@@ -1,67 +1,73 @@
 module ActionController
-  module Acts
-    module Containable  
-      module InstanceMethods
-        def create
-          record = klass.create_in_container(params[:parent_id], params[klass.name.downcase.to_sym])
+	module Acts
+		module Containable  
+			module InstanceMethods
+				def create
+					record = klass.create_in_container(params[:parent_id], params[klass.name.downcase.to_sym])
 
-          respond_to do |f|
-            if record.valid?
-              flash[:notice] = "You have just created a new #{klass.name}."
-              f.html {redirect_to record}
-            else
-              instance_variable_set("@#{klass.name.downcase.singularize}", record)
-              f.html {render :new}
-            end
-          end
-        end       
-        
-        def destroy
-          record = klass.send(find_method, params[:id])
-          container = record.container
+					respond_to do |f|
+						if record.valid?
+							flash_notice = "You have just created a new #{klass.name}."
+							f.html do
+								flash[:notice] = flash_notice
+								redirect_to record
+							end
+							f.js do
+								flash.now[:notice] = flash_notice
+							end
+						else
+							instance_variable_set("@#{klass.name.downcase.singularize}", record)
+							f.html {render :new}
+						end
+					end
+				end       
 
-          to = root_path
-          to = container.parent.containable if container.parent
+				def destroy
+					record = klass.send(find_method, params[:id])
+					container = record.container
 
-          respond_to do |f|
-            if record.destroy
-              flash[:notice] = "You just deleted the #{klass.name}."
-              f.html {redirect_to to}
-            else
-              flash[:error] = "There was a problem deleting the #{klass.name}. Please try again."
-              f.html {redirect_to record}
-            end
-          end
-        end
+					to = root_path
+					to = container.parent.containable if container.parent
 
-        # This has only been tested manually.
-        def sort
-          record = klass.send(find_method, params[:id])
-          parent_container = record.container
-          parent_container.children.each do |container|
-            container.position = params['container'].index(container.id.to_s) + 1
-            container.save
-          end
-          render :nothing => true
-        end
+					respond_to do |f|
+						if record.destroy
+							flash[:notice] = "You just deleted the #{klass.name}."
+							f.html {redirect_to to}
+						else
+							flash[:error] = "There was a problem deleting the #{klass.name}. Please try again."
+							f.html {redirect_to record}
+						end
+					end
+				end
 
-        protected
+				# This has only been tested manually.
+				def sort
+					record = klass.send(find_method, params[:id])
+					parent_container = record.container
+					parent_container.children.each do |container|
+						container.position = params['container'].index(container.id.to_s) + 1
+						container.save
+					end
+					render :nothing => true
+				end
 
-        def klass
-          self.class.acts_as_containable_config.model
-        end
+				protected
 
-        def find_method
-          "find_by_#{self.class.acts_as_containable_config.find_by}"
-        end
+				def klass
+					self.class.acts_as_containable_config.model
+				end
 
-        # Returns the children containers of this record's container.
-        def find_child_containers
-          record = klass.send(find_method, params[:id])
-          container = record.container
-          @child_containers ||= Container.where(:parent_id => container.id)
-        end
-      end             
-    end             
-  end             
-end             
+				def find_method
+					"find_by_#{self.class.acts_as_containable_config.find_by}"
+				end
+
+				# Returns the children containers of this record's container.
+				def find_child_containers
+					record = klass.send(find_method, params[:id])
+					container = record.container
+					@child_containers ||= Container.where(:parent_id => container.id)
+				end
+			end
+		end
+	end
+end
