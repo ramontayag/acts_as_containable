cb0c79cc397c8943f2a386c98d97c48fd3fd8a3c
Ramon Tayag <ramon@tayag.net>
2011-10-02 15:36:57 +0800

    Whitespace cleanup

--- lib/container.rb
+++ lib/container.rb
@@ -1,17 +1,17 @@
 class Container < ActiveRecord::Base
-	ordered_tree
-	belongs_to :containable, :polymorphic => true
-	before_destroy :destroy_containable
-	default_scope :order => "position"
+  ordered_tree
+  belongs_to :containable, :polymorphic => true
+  before_destroy :destroy_containable
+  default_scope :order => "position"
   named_scope :of_type, lambda { |type|
     {:conditions => {:containable_type => type} }
   }
 
-	private
+  private
 
-	def destroy_containable
-		if containable_type.constantize.exists?(containable_id)
-			containable_type.constantize.destroy(containable_id)
-		end
-	end
+  def destroy_containable
+    if containable_type.constantize.exists?(containable_id)
+      containable_type.constantize.destroy(containable_id)
+    end
+  end
 end
