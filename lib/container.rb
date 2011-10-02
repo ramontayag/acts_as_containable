1c5700d173336bd4b71df47d5f306fac5fcfd885
Ramon Tayag <ramon@tayag.net>
2011-10-02 15:36:45 +0800

    Uses ordered_tree instead of acts_as_ordered_tree

--- lib/container.rb
+++ lib/container.rb
@@ -1,5 +1,5 @@
 class Container < ActiveRecord::Base
-	acts_as_ordered_tree
+	ordered_tree
 	belongs_to :containable, :polymorphic => true
 	before_destroy :destroy_containable
 	default_scope :order => "position"
@@ -11,7 +11,7 @@ class Container < ActiveRecord::Base
 
 	def destroy_containable
 		if containable_type.constantize.exists?(containable_id)
-			containable_type.constantize.destroy(containable_id) 
+			containable_type.constantize.destroy(containable_id)
 		end
 	end
 end
